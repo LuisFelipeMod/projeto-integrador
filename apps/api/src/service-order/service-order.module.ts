5dc1cb6548f0adb5d6437a5d82906b70361eec79
Marcelo Magalhães <marcelo.maga2550@gmail.com>
2024-09-24 21:44:53 -0300

    hotfix: import prisma service in module

--- apps/api/src/service-order/service-order.module.ts
+++ apps/api/src/service-order/service-order.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { ServiceOrderService } from './service-order.service';
 import { ServiceOrderController } from './service-order.controller';
+import { PrismaService } from '../shared/services/database';
 
 @Module({
   controllers: [ServiceOrderController],
-  providers: [ServiceOrderService],
+  providers: [ServiceOrderService, PrismaService],
 })
 export class ServiceOrderModule {}

17931b39dddc8355c3fa7707cd0fd4967df0c65e
VictorNAGomes <dev.victorgomes@gmail.com>
2024-10-07 22:26:04 -0300

    feat: implement Company CRUD

--- apps/api/src/company/company.controller.ts
+++ apps/api/src/company/company.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import { Controller, Get, Post, Body, Patch, Param, Delete, BadRequestException, InternalServerErrorException, HttpException } from '@nestjs/common';
 import { CompanyService } from './company.service';
 import { CreateCompanyDto } from './dto/create-company.dto';
 import { UpdateCompanyDto } from './dto/update-company.dto';
@@ -8,8 +8,31 @@ export class CompanyController {
   constructor(private readonly companyService: CompanyService) {}
 
   @Post()
-  create(@Body() createCompanyDto: CreateCompanyDto) {
-    return this.companyService.create(createCompanyDto);
+  async create(@Body() createCompanyDto: CreateCompanyDto) {
+    try {
+      const createdCompany = await this.companyService.create(createCompanyDto);;
+
+      return {
+        message: 'Empresa criada com sucesso',
+        data: createdCompany,
+      };
+    } catch (error) {
+      if (error.code === 'P2002') {
+        throw new BadRequestException(
+          'Não foi possível criar empresa. (Os dados já estão sendo utilizados)',
+        );
+      }
+
+      console.log(error.response)
+      
+      if (error.response.statusCode != 500) {
+        throw new HttpException(error.response.message, error.response.statusCode);
+      }
+
+      throw new InternalServerErrorException(
+        'Algum erro inesperado aconteceu, tente novamente mais tarde',
+      );
+    }
   }
 
   @Get()
@@ -19,16 +42,16 @@ export class CompanyController {
 
   @Get(':id')
   findOne(@Param('id') id: string) {
-    return this.companyService.findOne(+id);
+    return this.companyService.findOne(id);
   }
 
   @Patch(':id')
   update(@Param('id') id: string, @Body() updateCompanyDto: UpdateCompanyDto) {
-    return this.companyService.update(+id, updateCompanyDto);
+    return this.companyService.update(id, updateCompanyDto);
   }
 
   @Delete(':id')
   remove(@Param('id') id: string) {
-    return this.companyService.remove(+id);
+    return this.companyService.remove(id);
   }
 }
--- apps/api/src/company/company.service.ts
+++ apps/api/src/company/company.service.ts
@@ -1,38 +1,78 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { CreateCompanyDto } from './dto/create-company.dto';
 import { UpdateCompanyDto } from './dto/update-company.dto';
 import { PrismaService } from "../shared/services/database";
 
 @Injectable()
 export class CompanyService {
-  constructor(private readonly prismaService: PrismaService) {}
+  constructor(
+    private readonly prismaService: PrismaService,
+    // private readonly serviceOrder: ServiceOrderService
+  ) {}
 
   async create(createCompanyDto: CreateCompanyDto) {
-    try {
+    const owner = await this.prismaService.user.findUnique({
+      where: { id: createCompanyDto.ownerId }
+    });
 
-      const company = await this.prismaService.company.create({
-        data: createCompanyDto,
-      }); 
-
-      return company;
-    } catch (error) {
-      console.log("teste: " + error);
+    if(!owner) {
+      throw new NotFoundException(
+        "Não foi possível criar empresa. (O usuário não existe)"
+      )
     }
+
+    const company = await this.prismaService.company.create({
+      data: createCompanyDto,
+    }); 
+
+    return company;
   }
 
-  findAll() {
-    return `This action returns all company`;
+  async findAll() {
+    const companies = await this.prismaService.company.findMany()
+    
+    if(!companies) {
+        throw new NotFoundException(
+          "Ainda não existe empresas cadastradas"
+        )
+    }
+
+    return companies;
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} company`;
+
+  async findOne(id: string) {
+    const company = await this.prismaService.company.findUnique({
+      where: {
+        id: id
+      }
+    })
+    
+    if(!company) {
+        throw new NotFoundException(
+          "Empresa não encontrada"
+        )
+    }
+
+    return company;
   }
 
-  update(id: number, updateCompanyDto: UpdateCompanyDto) {
-    return `This action updates a #${id} company`;
+  async update(id: string, updateCompanyDto: UpdateCompanyDto) {
+    const company = await this.findOne(id);
+
+    return await this.prismaService.company.update({
+      data: updateCompanyDto,
+      where: { id: id}
+    });
   }
 
-  remove(id: number) {
-    return `This action removes a #${id} company`;
+  async remove(id: string) {    
+    const company = await this.findOne(id);
+
+    await this.prismaService.company.delete({
+      where: { id: id}
+    });
+
+    return "Empresa Deletada com sucesso"
   }
 }
